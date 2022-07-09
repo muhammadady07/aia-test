1b1e05d48caca8f25f46abb8223774ca6ef4558b
Muhammad Ady <muhammadady07@gmail.com>
2022-07-10 01:58:58 +0700

    feat: fix eslint

--- routes/flickrPhoto/index.js
+++ routes/flickrPhoto/index.js
@@ -2,14 +2,6 @@ const { setHeader } = require('../../services/setHeader');
 const flickrPhotoHandler = require('../../api/flickrPhoto/index');
 
 module.exports = (app) => {
-  app.get(
-		'/api/flickr-photos',
-		setHeader,
-		flickrPhotoHandler.getAll,
-  );
-  app.get(
-		'/api/flickr-photos/search',
-		setHeader,
-		flickrPhotoHandler.getByTag,
-  );
+  app.get('/api/flickr-photos', setHeader, flickrPhotoHandler.getAll);
+  app.get('/api/flickr-photos/search', setHeader, flickrPhotoHandler.getByTag);
 };
