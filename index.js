f08b50eca7229c75970adb40d4a4c4ba49270d7c
Muhammad Ady <muhammadady07@gmail.com>
2022-07-10 09:28:29 +0700

    fix: CORS

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://aia-test-zeta.vercel.app',
+];
+
+// set allowed domain
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+        'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  }),
+);
+
 require('./routes/flickrPhoto/index')(app);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
